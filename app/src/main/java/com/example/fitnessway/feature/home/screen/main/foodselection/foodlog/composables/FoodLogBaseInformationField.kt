788e43a2c144881debb09791589ca54827f3cf77
Christian Lopez <christian.ylopez02@gmail.com>
2025-11-04 21:15:10 -0600

    improved food log form's fields UI edit mode

--- app/src/main/java/com/example/fitnessway/feature/home/screen/main/foodselection/foodlog/composables/FoodLogBaseInformationField.kt
+++ app/src/main/java/com/example/fitnessway/feature/home/screen/main/foodselection/foodlog/composables/FoodLogBaseInformationField.kt
@@ -1,17 +1,19 @@
 package com.example.fitnessway.feature.home.screen.main.foodselection.foodlog.composables
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
 import com.example.fitnessway.data.model.form.FoodLogField
 
@@ -23,21 +25,38 @@ fun FoodLogBaseInformationField(
     val borderWidth = if (isEditing) 1.dp else 0.dp
     val borderColor = if (isEditing) MaterialTheme.colorScheme.primary else Color.Transparent
     val borderShape = if (isEditing) 10.dp else 0.dp
-
     val padding = if (isEditing) 14.dp else 0.dp
+
+    val textStyle = TextStyle(
+        fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+        fontFamily = FontFamily.Default,
+        color = MaterialTheme.colorScheme.primary,
+    )
+
+    // Measure the actual text width
+    val textMeasurer = rememberTextMeasurer()
+    val density = LocalDensity.current
+
+    val measuredWidth = remember(field.value, textStyle) {
+        val textLayoutResult = textMeasurer.measure(
+            text = field.value.ifEmpty { "0" },
+            style = textStyle
+        )
+
+        // Convert pixels to dp correctly using density
+        with(density) {
+            textLayoutResult.size.width.toDp() + 16.dp
+        }
+    }
 
     BasicTextField(
         value = field.value,
         onValueChange = field.updateState,
-        textStyle = TextStyle(
-            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-            fontFamily = FontFamily.Default,
-            color = MaterialTheme.colorScheme.primary
-        ),
+        textStyle = textStyle,
         singleLine = true,
         enabled = isEditing,
         modifier = Modifier
-            .width(IntrinsicSize.Min)
+            .width(measuredWidth + padding)
             .border(
                 width = borderWidth,
                 color = borderColor,
